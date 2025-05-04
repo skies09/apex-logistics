0b9681ce93016b41cd1f7d2b0704c75586f6b4bd
Donna <skies09@hotmail.com>
2025-05-04 15:46:25 +0100

    update border

--- src/components/border.tsx
+++ src/components/border.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Border = () => {
 	return (
-		<div className="my-12">
-			<div className="border-t-4 border-white"></div>
+		<div className="my-6 lg:my-0">
+			<div className="border-t-4 border-colorThree"></div>
 
-			<div className="border-t-2 border-dashed border-gray-800 my-4"></div>
+			<div className="border-t-2 border-dashed border-colorThree my-4"></div>
 
-			<div className="border-t-4 border-gray-800"></div>
+			<div className="border-t-4 border-colorThree"></div>
 		</div>
 	);
 };
